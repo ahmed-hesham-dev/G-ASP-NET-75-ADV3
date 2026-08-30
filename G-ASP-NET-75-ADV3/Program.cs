04738bc923d99d62fa73bf4fa929f717c90fd5e4
Ahmed Hesham <ah0115915v@gmail.com>
2026-08-30 19:24:46 +0300

    Add project files.

---
+++ G-ASP-NET-75-ADV3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_ASP_NET_75_ADV3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
